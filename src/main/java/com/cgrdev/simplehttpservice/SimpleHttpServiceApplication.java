6207dca38d6a7a4f4b66b9e62e7d65b1a6ec59b5
garciacarlos78 <51118101+garciacarlos78@users.noreply.github.com>
2020-03-06 12:07:49 +0100

    Initial commit

---
+++ src/main/java/com/cgrdev/simplehttpservice/SimpleHttpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cgrdev.simplehttpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHttpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHttpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgrdev/simplehttpservice/SimpleHttpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgrdev.simplehttpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHttpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
