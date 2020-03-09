03b8b67469a6c22dda2bd6a8f9e67f917525be27
garciacarlos78 <carles.garcia4@hotmail.com>
2020-03-09 09:30:15 +0100

    Adding employee OK

--- src/main/resources/static/controller.js
+++ src/main/resources/static/controller.js
@@ -21,4 +21,27 @@ $(document).ready(function () {
             });
         }
     });
+    $('#add_one').click(function (){
+        if ($('#post_name').val() && $('#post_role').val()){
+//            alert("Name: " + $('#post_name').val() + "\nRole: " + $('#post_role').val());
+            $.ajax({
+                url: "/employees",
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify({"name": $('#post_name').val(),"role": $('#post_role').val()}),
+                processData: false,
+                success: function(employee){
+                    $('#result_add_one').empty();
+                    $('#result_add_one').append("Successfully added employee!!!<br>");
+                    $('#result_add_one').append("Id: " + employee.id + ", Name: " + employee.name + ", Role: " + employee.role);
+                },
+                error: function(stat) {
+                    alert(stat.responseText);
+                }
+            });
+        } else {
+            alert("Faltan campos!!!");
+        }
+    });
 });
