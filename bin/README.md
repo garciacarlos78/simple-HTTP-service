5dfd20ac789e8ca1bc96cd3e08a2ca00f2453784
garciacarlos78 <51118101+garciacarlos78@users.noreply.github.com>
2020-03-10 09:26:30 +0100

    Tested in classroom eclipse

---
+++ bin/README.md
@@ -0,0 +1,6 @@
+# IT Academy Simple HTTP service (RCP) exercise
+
+## Instructions
+Run the project in your favourite IDE and access http://localhost:8080/ in your favourite browser.
+
+**IMPORTANT:** You must have Lombok plugin installed in your IDE to get it working.
---
+++ bin/src/main/resources/static/controller.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+    $('#get_all').click(function (){
+        $.get("/employees",
+        function(employees) {
+            $('#result').empty();
+            $.each(employees, function(i, employee) {
+                $('#result').append("Id: " + employee.id + ", Name: " + employee.name + ", Role: " + employee.role + ", Salary: " + employee.salary + "k €<br>");
+            });
+        });
+    });
+    $('#get_one').click(function (){
+        if ($('#id_one').val()) {
+            var url="/employees/" + $('#id_one').val();
+            $.get(url,
+            function(employee) {
+                $('#result_get_one').empty();
+                $('#result_get_one').append("Id: " + employee.id + ", Name: " + employee.name + ", Role: " + employee.role + ", Salary: " + employee.salary + "k €<br>");
+            }).fail(function(stat){
+                $('#result_get_one').empty();
+                $('#result_get_one').append(stat.responseText);
+            });
+        }
+    });
+    $('#add_one').click(function (){
+        if ($('#post_name').val() && $('#post_role').val()){
+            $.ajax({
+                url: "/employees",
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify({"name": $('#post_name').val(),"role": $('#post_role').val()}),
+                processData: false,
+                success: function(employee){
+                    $('#result_add_one').empty();
+                    $('#result_add_one').append("Successfully added employee!!!<br>");
+                    $('#result_add_one').append("Id: " + employee.id + ", Name: " + employee.name + ", Role: " + employee.role + ", Salary: " + employee.salary + "k €<br>");
+                },
+                error: function(stat) {
+                    alert(stat.responseText);
+                }
+            });
+        } else {
+            alert("Please fill in name and role to create a new employee.");
+        }
+    });
+    $('#update').click(function (){
+        if ($('#id_put').val() && ($('#post_name').val() || $('#post_role').val())){
+            $.ajax({
+                url: "/employees/" + $('#id_put').val(),
+                dataType: "json",
+                type: "PUT",
+                contentType: "application/json",
+                data: JSON.stringify({"name": $('#post_name').val(),"role": $('#post_role').val()}),
+                processData: false,
+                success: function(employee){
+                    $('#result_add_one').empty();
+                    $('#result_add_one').append("Successfully updated employee!!!<br>");
+                    $('#result_add_one').append("Id: " + employee.id + ", Name: " + employee.name + ", Role: " + employee.role + ", Salary: " + employee.salary + "k €<br>");
+                },
+                error: function(stat) {
+                    $('#result_add_one').empty();
+                    $('#result_add_one').append(stat.responseText);
+                }
+            });
+        } else {
+            alert("Please fill in employee id and at least name or salary to update an employee.");
+        }
+    });
+    $('#delete').click(function (){
+        if ($('#id_put').val()){
+            $.ajax({
+                url: "/employees/" + $('#id_put').val(),
+                type: "DELETE",
+                success: function(){
+                    $('#result_add_one').empty();
+                    $('#result_add_one').append("Successfully deleted employee!!!<br>");
+                },
+                error: function(stat) {
+                    $('#result_add_one').empty();
+                    $('#result_add_one').append(stat.responseText);
+                }
+            });
+        }
+    });
+    $('#find_by_role').click(function (){
+        $.get("/employees/role/" + $('#post_role').val(),
+        function(employees) {
+            $('#result_add_one').empty();
+            $.each(employees, function(i, employee) {
+                $('#result_add_one').append("Id: " + employee.id + ", Name: " + employee.name + ", Role: " + employee.role + ", Salary: " + employee.salary + "k €<br>");
+            });
+        }).fail(function(stat){
+            $('#result_add_one').empty();
+            $('#result_add_one').append(stat.responseText);
+        });
+    });
+});
